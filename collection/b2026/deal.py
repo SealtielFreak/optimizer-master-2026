00b1a2b7fc32af745777ca9b9d346312595709c6
SealtielFreak <sealtiel@email.com>
2026-05-26 19:30:39 -0600

    Implemented differential evolution

--- collection/b2026/deal.py
+++ collection/b2026/deal.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 from mealpy.optimizer.classic import ClassicOptimizer
@@ -8,7 +10,7 @@ from scipy.stats import wilcoxon
 
 
 class DEAL(ClassicOptimizer):
-    def __init__(self, epoch: int = 10000, pop_size: int = 100, c1: float = 2.05, c2: float = 2.05, minimum_pop: int = 3,
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, c1: float = 2.05, c2: float = 2.05, minimum_pop: int = 4, wf: float = 0.1,
                  **kwargs: object) -> None:
         """
         Args:
@@ -27,11 +29,14 @@ class DEAL(ClassicOptimizer):
         self.w_stats = 1
         self.p = 1
 
-        self.minimum_pop = self.validator.check_int("minimum_pop", minimum_pop, [1, 100000])
+        self.minimum_pop = self.validator.check_int("minimum_pop", minimum_pop, [4, 100000])
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
         self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.c1 = self.validator.check_float("c1", c1, (0, 5.0))
         self.c2 = self.validator.check_float("c2", c2, (0, 5.0))
+
+        self.wf = self.validator.check_float("wf", wf, (-3.0, 3.0))
+
         self.set_parameters(["epoch", "pop_size", "c1", "c2"])
         self.sort_flag = False
         self.is_parallelizable = False
@@ -77,7 +82,6 @@ class DEAL(ClassicOptimizer):
 
         # Update weight after each move count  (weight down)
 
-
         if len(self.history_best_pop) > 1:
             self.b_stats = wilcoxon([p.target.fitness for p in self.history_best_pop]).pvalue
 
@@ -86,10 +90,7 @@ class DEAL(ClassicOptimizer):
 
         current_pop_len = len(self.pop)
 
-        if self.b_stats < 0.05 and current_pop_len > self.minimum_pop:
-            self.pop = self.get_sorted_population(self.pop)[::-1]
-            self.history_worst_pop += [self.pop[0]]
-
+        if self.b_stats < 0.05 and current_pop_len > self.minimum_pop and epoch > self.epoch // 3:
             self.pop.pop(0)
             self.pop_size -= 1
         elif current_pop_len < self.default_pop_size:
@@ -97,20 +98,23 @@ class DEAL(ClassicOptimizer):
             self.pop_size += 1
 
         if self.b_stats < self.w_stats:
-            self.p *= self.b_stats
-        else:
-            self.p *= self.w_stats
+            self.p *= math.fabs(self.b_stats - self.w_stats)
+
+        print("W - Exploration" if self.w_stats > self.b_stats else "B - Explotation")
+        print(self.b_stats, self.w_stats, math.fabs(self.b_stats - self.w_stats), self.p)
 
         pos_new_solutions = []
 
         for idx in range(0, self.pop_size):
-            cognitive = self.c1 * self.generator.random(self.problem.n_dims) * (self.pop[idx].local_solution - self.pop[idx].solution)
-            social = self.c2 * self.generator.random(self.problem.n_dims) * (self.g_best.solution - self.pop[idx].solution)
+            # cognitive = self.c1 * self.generator.random(self.problem.n_dims) * (self.pop[idx].local_solution - self.pop[idx].solution)
+            # social = self.c2 * self.generator.random(self.problem.n_dims) * (self.g_best.solution - self.pop[idx].solution)
 
-            self.pop[idx].velocity = (self.p * self.pop[idx].velocity) + cognitive + social
+            self.pop[idx].velocity = (self.p * self.pop[idx].velocity) #  + cognitive + social
 
             # pos_new = self.pop[idx].solution + self.pop[idx].velocity
-            pos_new = self.pop[idx].solution + self.pop[idx].velocity
+
+            idx_list = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}), 3, replace=False)
+            pos_new = (self.pop[idx_list[0]].solution + self.wf * (self.pop[idx_list[1]].solution - self.pop[idx_list[2]].solution)) + self.pop[idx].velocity
 
             pos_new = self.correct_solution(pos_new)
             pos_new_solutions.append(pos_new)
@@ -134,6 +138,9 @@ class DEAL(ClassicOptimizer):
                     local_target=target.copy()
                 )
 
-        gbest = self.get_best_agent(self.pop, self.problem.minmax)
-        self.history_best_pop += [gbest]
-        self.gbest = sorted(self.history_best_pop, key=lambda p: p.target.fitness)[0]
+        self.pop = self.get_sorted_population(self.pop, self.problem.minmax)
+
+        self.history_best_pop += [self.pop[0]]
+        self.history_worst_pop += [self.pop[-1]]
+
+        self.gbest = self.get_sorted_population(self.pop, self.problem.minmax)[0]
