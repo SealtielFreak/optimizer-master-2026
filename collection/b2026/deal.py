af9d8e5cbd526d3ecd4cd0f097f4552760b874b0
Sealtiel <sealtiel@email.com>
2026-05-27 09:29:29 -0600

    Strategies like JADE and SHADED

--- collection/b2026/deal.py
+++ collection/b2026/deal.py
@@ -1,14 +1,18 @@
-import math
-
 import numpy as np
 
 from mealpy.optimizer.classic import ClassicOptimizer
 from mealpy.utils.agent import Agent
 
-from scipy.stats import wilcoxon, norm, mode, cauchy
+from scipy.stats import wilcoxon, cauchy
 
 
 class DEAL(ClassicOptimizer):
+    """
+    Differential Evolution Adaptative Lineal
+    """
+
+    STATS_MODES = 'mode', 'sorted', 'median', 'mean'
+
     def __init__(
             self,
             epoch: int = 10000,
@@ -17,6 +21,7 @@ class DEAL(ClassicOptimizer):
             miu_f: float = 0.5,
             miu_cr: float = 0.5,
             cr: float = 0.9,
+            ap: float = 0.1,
             stats_mode: str = "sorted",
             **kwargs: object
     ) -> None:
@@ -29,7 +34,7 @@ class DEAL(ClassicOptimizer):
         super().__init__(**kwargs)
 
         self.all_history_best_pop = []
-        self.all_history_worst_pop = []
+        self.all_history_wort_pop = []
 
         self.counter_pop_tolerance = 10
         self.counter_pop = 0
@@ -39,15 +44,15 @@ class DEAL(ClassicOptimizer):
         self.impulse = 1
 
         self.g_best_history = None
-        self.g_worst_history = None
 
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
         self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.miu_f = self.validator.check_float("miu_f", miu_f, (0, 1.0))
         self.miu_cr = self.validator.check_float("miu_cr", miu_cr, (0, 1.0))
         self.cr = self.validator.check_float("cr", cr, (0, 1.0))
+        self.ap = self.validator.check_float("ap", ap, (0, 1.0))
         self.minimum_pop = self.validator.check_int("minimum_pop", minimum_pop, [4, 100000])
-        self.stats_mode = self.validator.check_str("stats_mode", stats_mode, ['mode', 'sorted', 'median', 'mean'])
+        self.stats_mode = self.validator.check_str("stats_mode", stats_mode, self.STATS_MODES)
 
         self.set_parameters(["epoch", "pop_size", "miu_f", "miu_cr", "cr", "minimum_pop", "stats_mode"])
 
@@ -56,11 +61,11 @@ class DEAL(ClassicOptimizer):
         self.v_max = 0
         self.v_min = 0
 
+        self.dyn_miu_cr = 0
+        self.dyn_miu_f = 0
+
         self.dyn_pop_archive = []
-        self.best_pop_fitness = []
-        self.wors_pop_fitness = []
         self.current_history_best_pop = []
-        self.current_history_worst_pop = []
 
         self.sort_flag = False
         self.is_parallelizable = False
@@ -68,11 +73,13 @@ class DEAL(ClassicOptimizer):
     def mutation(self, current_pos, new_pos):
         condition = self.generator.random(self.problem.n_dims) < self.cr
         pos_new = np.where(condition, new_pos, current_pos)
+
         return self.correct_solution(pos_new)
 
     def initialize_variables(self):
         self.dyn_miu_cr = self.miu_cr
         self.dyn_miu_f = self.miu_f
+        self.dyn_pop_archive = list()
 
     def generate_empty_agent(self, solution: np.ndarray = None) -> Agent:
         if solution is None:
@@ -97,65 +104,42 @@ class DEAL(ClassicOptimizer):
         return np.where(condition, solution, pos_rand)
 
     def evolve(self, epoch):
-        """
-        The main operations (equations) of algorithm. Inherit from Optimizer class
+        list_f = []
+        list_cr = []
+        temp_f = []
+        temp_cr = []
 
-        Args:
-            epoch (int): The current iteration
-        """
-
-        # Update weight after each move count  (weight down)
-
-        list_f = list()
-        list_cr = list()
-        temp_f = list()
-        temp_cr = list()
-
-        self.best_pop_fitness = []
-        self.wors_pop_fitness = []
         self.current_history_best_pop = []
-        self.current_history_worst_pop = []
 
         if len(self.all_history_best_pop) > 5:
             self.current_history_best_pop = self.get_sorted_population(self.all_history_best_pop, self.problem.minmax)
             self.g_best_history = self.current_history_best_pop[0].copy()
 
-            self.best_pop_fitness = [p.target.fitness for p in self.all_history_best_pop]
-            self.b_stats = wilcoxon(self.best_pop_fitness).pvalue
+            best_pop_fitness = [p.target.fitness for p in self.all_history_best_pop]
+            self.b_stats = wilcoxon(best_pop_fitness).pvalue
         else:
             self.g_best_history = self.g_best
 
-        if len(self.all_history_worst_pop) > 5:
-            self.current_history_worst_pop = self.get_sorted_population(self.all_history_worst_pop, self.problem.minmax)[::-1]
-            self.g_worst_history = self.current_history_worst_pop[0].copy()
-
-            self.wors_pop_fitness = [p.target.fitness for p in self.all_history_worst_pop]
-            self.w_stats = wilcoxon(self.wors_pop_fitness).pvalue
-        else:
-            self.g_worst_history = self.g_worst
-
         current_pop_len = len(self.pop)
 
+        g_best_mode_solution = self.g_best_history.solution
+        if len(self.current_history_best_pop) > (self.default_pop_size / 3) and self.mode in self.STATS_MODES:
+            g_best_mode_solution = stats_solution(self.mode,
+                                                  np.array([p.solution for p in self.current_history_best_pop]))
+
+        self.pop = self.get_sorted_population(self.pop, self.problem.minmax)
+
         if self.b_stats < 0.05 and current_pop_len > self.minimum_pop and epoch > self.epoch // 3:
-            self.pop.pop(0)
+            self.all_history_wort_pop += [self.pop[-1].copy()]
             self.pop_size -= 1
             self.counter_pop += 1
+
+            self.pop.pop(-1)
         elif current_pop_len < self.default_pop_size:
-            self.pop += [self.generate_agent()]
+            p = self.all_history_wort_pop[0].copy()
+            self.all_history_wort_pop.pop(0)
+            self.pop += [p]
             self.pop_size += 1
-
-        # print(self.b_stats, self.w_stats)
-
-        g_best_mode_solution = self.g_best_history.solution
-        if len(self.current_history_best_pop) > (self.default_pop_size / 3):
-            all_g_best_solutions = np.array([p.solution for p in self.current_history_best_pop])
-
-            if self.stats_mode == 'mode':
-                g_best_mode_solution = mode(all_g_best_solutions, keepdims=True)[0][0]
-            elif self.stats_mode == 'median':
-                g_best_mode_solution = np.median(all_g_best_solutions, axis=0)
-            elif self.stats_mode == 'mean':
-                g_best_mode_solution = np.mean(all_g_best_solutions, axis=0)
 
         pos_new_solutions = []
         for idx in range(0, self.pop_size):
@@ -164,23 +148,23 @@ class DEAL(ClassicOptimizer):
 
             while True:
                 f = cauchy.rvs(self.dyn_miu_f, 0.1)
+
                 if f < 0:
                     continue
                 elif f > 1:
                     f = 1
                 break
 
-            temp_f.append(f)
-            temp_cr.append(cr)
+            temp_f += [f]
+            temp_cr += [cr]
 
-            idx_list = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}), 2, replace=False)
+            r1_idx, r2_idx = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}), 2, replace=False)
+            x_new = (
+                (self.pop[idx].solution + f * (g_best_mode_solution - self.pop[idx].solution) + f * (
+                            self.pop[r1_idx].solution - self.pop[r2_idx].solution))
+            )
 
-            """
-            Reimplementation of evolution strategy
-            """
-            pos_new = ((self.pop[idx].solution + f * (g_best_mode_solution - self.pop[idx].solution) + f * (self.pop[idx_list[0]].solution - self.pop[idx_list[1]].solution)))
-            pos_new = self.mutation(self.pop[idx].solution, pos_new)
-
+            pos_new = self.mutation(self.pop[idx].solution, x_new)
             pos_new = self.correct_solution(pos_new)
             pos_new_solutions.append(pos_new)
 
@@ -196,19 +180,45 @@ class DEAL(ClassicOptimizer):
                     target=target.copy()
                 )
 
-            if self.compare_target(target, self.pop[idx].local_target, self.problem.minmax):
-                self.pop[idx].update(
-                    local_solution=pos_new.copy(),
-                    local_target=target.copy()
-                )
+                list_cr.append(temp_cr[idx])
+                list_f.append(temp_f[idx])
 
-            self.dyn_pop_archive.append(self.pop[idx].copy())
-
-            list_cr.append(temp_cr[idx])
-            list_f.append(temp_f[idx])
+        self.dyn_miu_cr, self.dyn_miu_f = update_historial(
+            self.dyn_miu_cr, self.dyn_miu_f, self.ap, list_cr, list_f,
+        )
 
         self.pop = self.get_sorted_population(self.pop, self.problem.minmax)
         self.g_best = self.pop[0].copy()
-
         self.all_history_best_pop += [self.g_best.copy()]
-        self.all_history_worst_pop += [self.pop[-1].copy()]
+
+
+def stats_solution(mode, arr):
+    all_modes = {
+        'mode': lambda s: mode(s, keepdims=True)[0][0],
+        'median': lambda s: np.median(s, axis=0),
+        'mean': lambda s: mode(s, axis=0),
+    }
+
+    if f_mode := all_modes.get(mode):
+        return f_mode(arr)
+
+    raise ValueError("Invalid mode")
+
+
+def lehmer_mean(list_objects):
+    temp = np.sum(list_objects)
+    return 0 if temp == 0 else np.sum(list_objects ** 2) / temp
+
+
+def update_historial(dyn_miu_cr, dyn_miu_f, ap, list_cr, list_f):
+    if len(list_cr) == 0:
+        dyn_miu_cr = (1 - ap) * dyn_miu_cr + ap * 0.5
+    else:
+        dyn_miu_cr = (1 - ap) * dyn_miu_cr + ap * np.mean(np.array(list_cr))
+
+    if len(list_f) == 0:
+        dyn_miu_f = (1 - ap) * dyn_miu_f + ap * 0.5
+    else:
+        dyn_miu_f = (1 - ap) * dyn_miu_f + ap * lehmer_mean(np.array(list_f))
+
+    return dyn_miu_cr, dyn_miu_cr
